130ea344708e8b6307356b1aa07199979180452b
Denis Orlov <des2033@gmail.com>
2026-01-02 21:52:23 +0400

    Добавил ÐФикс стиляÐ

--- src/main/java/functions/BadSleepSessionsCounter.java
+++ src/main/java/functions/BadSleepSessionsCounter.java
@@ -5,7 +5,6 @@ import ru.yandex.practicum.sleeptracker.SleepingSession;
 
 import java.time.Duration;
 import java.util.List;
-import java.util.OptionalDouble;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
